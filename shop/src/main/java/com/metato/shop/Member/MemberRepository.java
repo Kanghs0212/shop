80822a45da5b1e2e8d50a53121e74ac3d2251648
Metato <160302877+Kanghs0212@users.noreply.github.com>
2026-03-05 17:16:15 +0900

    Add files via upload

---
+++ shop/src/main/java/com/metato/shop/Member/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.metato.shop.Member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
+    Optional<Member> findByDisplayName(String displayName);
+    Optional<Member> findByEmail(String email);
+}
